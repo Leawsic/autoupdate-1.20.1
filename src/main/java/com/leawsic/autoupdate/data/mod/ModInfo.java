4193dbfb9bc9c5d8bab0a90b605c98b07ff5684d
Leawsic <3775717540@qq.com>
2026-02-12 13:27:18 +0800

    update ModInfo model to adapt to current mods list structure

--- src/main/java/com/leawsic/autoupdate/data/mod/ModInfo.java
+++ src/main/java/com/leawsic/autoupdate/data/mod/ModInfo.java
@@ -1,24 +1,44 @@
 package com.leawsic.autoupdate.data.mod;
 
 import com.google.gson.JsonObject;
+import com.leawsic.autoupdate.AutoUpdate;
+import com.leawsic.autoupdate.utils.HashCodeGenerator;
 import net.fabricmc.loader.api.ModContainer;
 import net.fabricmc.loader.api.metadata.ModMetadata;
+
+import java.io.IOException;
+import java.nio.file.Path;
 
 public class ModInfo {
     private final String id;
     private final String name;
     private final String version;
+    private final String sha1;
 
-    public ModInfo(ModContainer modContainer){
+    public ModInfo(ModContainer modContainer) {
         ModMetadata metadata=modContainer.getMetadata();
         this.id=metadata.getId();
         this.name=metadata.getName();
         this.version=metadata.getVersion().getFriendlyString();
+        this.sha1=getHashCode(modContainer);
     }
-    public ModInfo(String id,String name,String version){
+    public ModInfo(String id, String name, String version, String sha1){
         this.id=id;
         this.name=name;
         this.version=version;
+        this.sha1 = sha1;
+    }
+    private String getHashCode(ModContainer container) {
+        for (Path path:container.getOrigin().getPaths()){
+            if (path.toString().endsWith(".jar")){
+                try {
+                    return HashCodeGenerator.getSha1FromPath(path);
+                } catch (IOException e) {
+                    AutoUpdate.LOGGER.error("Failed to get mods' hashes");
+                }
+            }
+        }
+        return "";
     }
 
     public String getId() {
@@ -33,11 +53,16 @@ public class ModInfo {
         return version;
     }
 
+    public String getSha1(){
+        return sha1;
+    }
+
     public JsonObject toJsonObject(){
         JsonObject jsonObject=new JsonObject();
         jsonObject.addProperty("id",this.id);
         jsonObject.addProperty("name",this.name);
         jsonObject.addProperty("version",this.version);
+        jsonObject.addProperty("sha1",this.sha1);
         return jsonObject;
     }
 }
