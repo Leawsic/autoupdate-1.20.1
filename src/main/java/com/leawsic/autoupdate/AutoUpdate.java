0b2ca3bc70d55f81bfbec2dfc5e2d4d78f0fa1d1
Leawsic <3775717540@qq.com>
2026-02-14 21:25:11 +0800

    refactor(autoupdate): 更新版本号for 0.0.3 release

--- src/main/java/com/leawsic/autoupdate/AutoUpdate.java
+++ src/main/java/com/leawsic/autoupdate/AutoUpdate.java
@@ -7,7 +7,7 @@ import org.slf4j.LoggerFactory;
 public class AutoUpdate implements ModInitializer {
 	public static final String MOD_ID = "autoupdate";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-    public static final String MOD_VER = "0.0.2";
+    public static final String MOD_VER = "0.0.3";
 
     @Override
 	public void onInitialize() {
